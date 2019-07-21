7056c0b0c77226e180654f5715b9acbed10f76d3
Federico <fkatzaroff@gmail.com>
2019-07-21 19:10:36 -0300

    .

--- Login/ListForm.cs
+++ Login/ListForm.cs
@@ -11,14 +11,6 @@ namespace Login
         {
             this.usersController = usersController;
             InitializeComponent();
-            foreach(User user in usersController.ListUsers())
-            {
-                ListViewItem item = new ListViewItem();
-                item.Text = user.Username;
-                item.SubItems.Add(user.Fullname);
-                item.SubItems.Add(user.Email);
-                listUsers.Items.Add(item);
-            }
             grdUsers.DataSource = usersController.ListUsers();
         }
 
